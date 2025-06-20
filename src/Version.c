0b3f04264c2641fc48504063810d02ccff129e73
SuperPuiu <SuperPuiu@proton.me>
2025-06-20 20:44:38 +0300

    version: make use of epic Error function and also make the functions return a status code.

--- src/Version.c
+++ src/Version.c
@@ -1,6 +1,6 @@
 #include <Shared.h>
 
-void GetVersionData(VersionData *Data) {
+int8_t GetVersionData(VersionData *Data) {
   MemoryStruct Chunk;
   Chunk.Memory = malloc(1);
   Chunk.Size = 0;
@@ -8,69 +8,68 @@ void GetVersionData(VersionData *Data) {
   CURLcode Response = CurlGet(&Chunk, CLIENT_SETTINGS_URL);
   char **DecodedStrings;
   uint32_t NumberOfStrings = 0;
-  
+
   DecodedStrings = malloc(6 * sizeof(char *));
-  
-  if (!DecodedStrings) {
-    printf("[FATAL]: Unable to allocate DecodedStrings buffer in GetVersionData function.");
-    exit(EXIT_FAILURE);
-  }
+
+  if (!DecodedStrings)
+    Error("[FATAL]: Unable to allocate DecodedStrings buffer in GetVersionData function.", NULL, ERR_MEMORY);
 
   if (Response != CURLE_OK) {
-    printf("[ERROR]: GetVersionData error: %s\n", curl_easy_strerror(Response));
-    exit(EXIT_FAILURE);
-  } else {
-    for (size_t i = 1; i < Chunk.Size; i++) {
-      if (Chunk.Memory[i] == '"') {
-        char *String;
-        uint32_t SpaceRequired = 0, Position = i + 1;
-        i++;
+    Error("[ERROR]: GetVersionData failed downloading the client settings.", (char*)curl_easy_strerror(Response), ERR_STANDARD | ERR_NOEXIT);
+    free(DecodedStrings);
+    return -1;
+  }
 
-        /* First find out how much we have to allocate */
-        do {
-          SpaceRequired++;
-          Position++;
-        } while (Chunk.Memory[Position] != '"');
-        
-        String = calloc(SpaceRequired + 1, sizeof(char));
+  for (size_t i = 1; i < Chunk.Size; i++) {
+    if (Chunk.Memory[i] == '"') {
+      char *String;
+      uint32_t SpaceRequired = 0, Position = i + 1;
+      i++;
 
-        if (!String) {
-          printf("[FATAL]: Not enough memory to allocate String space in GetVersionData function.\n");
-          exit(EXIT_FAILURE);
-        }
-        
-        /* Store the found string. It doesn't have to be NULL byte terminated because calloc sets all bits to 0. */
-        memcpy(String, Chunk.Memory + i, SpaceRequired);
-        i += SpaceRequired;
-        
-        DecodedStrings[NumberOfStrings] = String;
-        NumberOfStrings++;
-      }
+      /* First find out how much we have to allocate */
+      do {
+        SpaceRequired++;
+        Position++;
+      } while (Chunk.Memory[Position] != '"');
+
+      String = malloc((SpaceRequired + 1) * sizeof(char));
+
+      if (!String) 
+        Error("[FATAL]: Not enough memory to allocate String space in GetVersionData function.", NULL, ERR_MEMORY);
+      
+      /* Store the found string. It doesn't have to be NULL byte terminated because calloc sets all bits to 0. */
+      memcpy(String, Chunk.Memory + i, SpaceRequired);
+      String[SpaceRequired] = '\0';
+
+      i += SpaceRequired;
+      
+      DecodedStrings[NumberOfStrings] = String;
+      NumberOfStrings++;
     }
   }
 
   Data->Version = DecodedStrings[1];
   Data->ClientVersionUpload = DecodedStrings[3];
   Data->BootstrapperVersion = DecodedStrings[5];
-  
+
   /* Cleanup. */
   for (size_t i = 0; i < NumberOfStrings; i += 2)
     free(DecodedStrings[i]);
   free(DecodedStrings);
 
   free(Chunk.Memory);
+
+  return 0;
 }
 
 /* Could be useful for testing if the CDN works. */
-void GetCDNVersion(MemoryStruct *VersionStruct) {
+int8_t GetCDNVersion(MemoryStruct *VersionStruct) {
   CURLcode Response;
   size_t LengthURL = strlen(CDN_URL), LengthVersion = strlen("version");
   char *FullURL = malloc((LengthURL + LengthVersion + 1) * sizeof(char)); /* One extra byte for null terminator. */
   
-  if (!FullURL) {
-    printf("[FATAL]: Unable to allocate FullURL in GetVersion function.\n");
-    exit(EXIT_FAILURE);
-  }
+  if (!FullURL)
+    Error("[FATAL]: Unable to allocate FullURL in GetVersion function.\n", NULL, ERR_MEMORY);
 
   VersionStruct->Memory = malloc(1);
   VersionStruct->Size = 0;
@@ -81,6 +80,13 @@ void GetCDNVersion(MemoryStruct *VersionStruct) {
 
   Response = CurlGet(VersionStruct, FullURL);
 
-  if (Response != CURLE_OK) { printf("GetVersion failed: %s\n", curl_easy_strerror(Response)); }
+  if (Response != CURLE_OK) { 
+    Error("[ERROR]: GetCDNVersion failed with curl error.\n", (char *)curl_easy_strerror(Response), ERR_STANDARD | ERR_NOEXIT);
+    free(FullURL);
+    return -1; 
+  }
+
   free(FullURL);
+
+  return 0;
 }
