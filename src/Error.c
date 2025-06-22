c9f4942cce74af673f529ca390dd5cf0accbc1bd
SuperPuiu <SuperPuiu@proton.me>
2025-06-22 21:52:16 +0300

    error: fix error logging (appears that my joke comment was real)

--- src/Error.c
+++ src/Error.c
@@ -6,7 +6,7 @@
 #include <time.h>
 
 /* TODO: https://unix.stackexchange.com/questions/104936/where-are-all-the-posibilities-of-storing-a-log-file */
-#define LOG_FILE          "/local/state/PwootieLog.log"
+#define LOG_FILE          "/.local/state/PwootieLog.log"
 #define STANDARD_FORMAT   "%s | %s\n"
 #define EXTENDED_FORMAT   "%s | %s (errno: %s)\n"
 
@@ -34,15 +34,20 @@ void Error(char *String, char *Additional, uint8_t Flags) {
 
   /* All day and night I'm dreaming memcpy(). */
   memcpy(Path, getenv("HOME"), HomeLength);
-  memcpy(Path + HomeLength + 1, LOG_FILE, Locationlength);
-  Path[HomeLength + Locationlength + 1] = '\0';
+  memcpy(Path + HomeLength, LOG_FILE, Locationlength);
+  Path[HomeLength + Locationlength] = '\0';
 
-  FILE *Debug = fopen(Path, "w");
+  FILE *Debug = fopen(Path, "a");
   
-  /* Imagine a situation where the Debug file can't be opened. */
+  if (!Debug)
+    Debug = fopen(Path, "w");
+
+  /* Imagine a situation where the Debug file can't be opened at all. */
   if (Debug) {
     fprintf(Debug, Additional != NULL ? EXTENDED_FORMAT : STANDARD_FORMAT, TimeBuffer, String, Additional != NULL ? Additional : "");
     fclose(Debug);
+  } else {
+    printf("[ERROR]: Unable to open PwootieLog.log. (errno: %s)\n", strerror(errno));
   }
   
   /* Clear errno. */
