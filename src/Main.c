d10787a3da9b17e40428bd98425485ad3cf4b939
SuperPuiu <SuperPuiu@proton.me>
2025-07-10 18:44:32 +0300

    main: pass StudioVersion directly to functions, make use of forced_version entry

--- src/Main.c
+++ src/Main.c
@@ -5,42 +5,51 @@ int main(int argc, char **argv) {
   curl_global_init(CURL_GLOBAL_ALL);
 
   VersionData Data;
+  char *StudioVersion = Data.ClientVersionUpload;
+  char *ForcedVersion = PwootieReadEntry("forced_version");
+  
+  /* If we have a ForcedVersion, then use that. */
+  StudioVersion = ForcedVersion ? ForcedVersion : StudioVersion;
 
   SetupHandles();
   GetVersionData(&Data);
 
   if (argc > 1) {
     if (strcmp(argv[1], "reinstall") == 0) {
-      Install(&Data, 0);
-      goto exit;
-    } else if (strcmp(argv[1], "proton") == 0) {
-      SetupProton();
+      if (strcmp(argv[2], "proton") == 0)
+        SetupProton(0);
+      else if (strcmp(argv[2], "studio"))
+        Install(StudioVersion, 0);
+      else
+        printf("[INFO]: Unknown reinstall option. (available options: proton, studio)\n");
+      
       goto exit;
     } else if (strcmp(argv[1], "fflags") == 0) {
-      if (LoadFFlags(Data.ClientVersionUpload) == -1) {
-        printf("[INFO]: Failed to load fflags.\n");
+      if (LoadFFlags(StudioVersion) == -1) {
+        Error("[INFO]: Failed to load fflags.\n", ERR_STANDARD | ERR_NOEXIT);
         goto exit;
       }
       
       if (argc == 2) {
-        printf("[INFO]: fflags require one or more parameters. See the documentation for more information\n");
+        printf("[INFO]: fflags require one or more parameters. See the documentation for more information.\n");
         goto exit;
       }
       
-      if (strcmp(argv[2], "apply") == 0) {
+      if (strcmp(argv[2], "apply") == 0)
         ApplyFFlag(argv[3], argv[4]);
-      } else if (strcmp(argv[2], "read") == 0) {
-        ReadFFlag(argv[3]);
-      }
+      else if (strcmp(argv[2], "read") == 0)
+        OutputFFlags(argv[3]);
+      else
+        printf("[INFO]: Unknown fflags option. (available options: apply, read)\n");
 
       goto exit;
     }
 
     /* The first argument can be the token we need to log into studio. */
-    Run(argv[1], Data.ClientVersionUpload);
+    Run(argv[1], StudioVersion);
   } else {
-    Install(&Data, 1);
-    Run(NULL, Data.ClientVersionUpload);
+    Install(StudioVersion, 1);
+    Run(NULL, StudioVersion);
   }
 
 exit:
@@ -48,6 +57,9 @@ exit:
   free(Data.Version);
   free(Data.ClientVersionUpload);
   free(Data.BootstrapperVersion);
+  
+  if (ForcedVersion)
+    free(ForcedVersion);
 
   curl_global_cleanup();
   PwootieExit();
