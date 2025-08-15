b4c7b02e947c4b95f153509d87ecb07f3a93a683
SuperPuiu <SuperPuiu@proton.me>
2025-08-15 20:12:50 +0300

    version: refactore for better speed

--- src/Version.c
+++ src/Version.c
@@ -11,53 +11,56 @@ int8_t GetVersionData(VersionData *Data) {
 
   DecodedStrings = malloc(6 * sizeof(char *));
 
-  if (!DecodedStrings)
+  if (unlikely(!DecodedStrings))
     Error("[FATAL]: Unable to allocate DecodedStrings buffer in GetVersionData function.", ERR_MEMORY);
 
-  if (Response != CURLE_OK) {
+  if (unlikely(Response != CURLE_OK)) {
     Error("[ERROR]: GetVersionData failed downloading the client settings.", ERR_STANDARD | ERR_NOEXIT);
     Error((char*)curl_easy_strerror(Response), ERR_STANDARD | ERR_NOEXIT);
     free(DecodedStrings);
     return -1;
   }
 
-  for (size_t i = 1; i < Chunk.Size; i++) {
+  for (uint32_t i = 1; i < Chunk.Size; i++) {
     if (Chunk.Memory[i] == '"') {
-      char *String;
-      uint32_t SpaceRequired = 0, Position = i + 1;
-      i++;
+      if (NumberOfStrings % 2 == 0) {
+        i++;
 
-      /* First find out how much we have to allocate */
-      do {
-        SpaceRequired++;
-        Position++;
-      } while (Chunk.Memory[Position] != '"');
+        do { i++; } while (Chunk.Memory[i] != '"');
+        NumberOfStrings++;
+      } else {
+        char *String;
+        uint32_t SpaceRequired = 0, Position = i + 1;
+        i++;
 
-      String = malloc((SpaceRequired + 1) * sizeof(char));
+        /* First find out how much we have to allocate */
+        do {
+          SpaceRequired++;
+          Position++;
+        } while (Chunk.Memory[Position] != '"');
 
-      if (!String) 
-        Error("[FATAL]: Not enough memory to allocate String space in GetVersionData function.", ERR_MEMORY);
-      
-      /* Store the found string. It doesn't have to be NULL byte terminated because calloc sets all bits to 0. */
-      memcpy(String, Chunk.Memory + i, SpaceRequired);
-      String[SpaceRequired] = '\0';
+        String = malloc((SpaceRequired + 1) * sizeof(char));
 
-      i += SpaceRequired;
-      
-      DecodedStrings[NumberOfStrings] = String;
-      NumberOfStrings++;
+        if (unlikely(!String)) 
+          Error("[FATAL]: Not enough memory to allocate String space in GetVersionData function.", ERR_MEMORY);
+
+        /* Store the found string. */
+        memcpy(String, Chunk.Memory + i, SpaceRequired);
+        String[SpaceRequired] = '\0';
+
+        i += SpaceRequired;
+
+        DecodedStrings[NumberOfStrings / 2] = String;
+        NumberOfStrings++;
+      }
     }
   }
 
-  Data->Version = DecodedStrings[1];
-  Data->ClientVersionUpload = DecodedStrings[3];
-  Data->BootstrapperVersion = DecodedStrings[5];
+  Data->Version = DecodedStrings[0];
+  Data->ClientVersionUpload = DecodedStrings[1];
+  Data->BootstrapperVersion = DecodedStrings[2];
 
-  /* Cleanup. */
-  for (size_t i = 0; i < NumberOfStrings; i += 2)
-    free(DecodedStrings[i]);
   free(DecodedStrings);
-
   free(Chunk.Memory);
 
   return 0;
@@ -69,7 +72,7 @@ int8_t GetCDNVersion(MemoryStruct *VersionStruct) {
   size_t LengthURL = strlen(CDN_URL), LengthVersion = strlen("version");
   char *FullURL = malloc((LengthURL + LengthVersion + 1) * sizeof(char)); /* One extra byte for null terminator. */
   
-  if (!FullURL)
+  if (unlikely(!FullURL))
     Error("[FATAL]: Unable to allocate FullURL in GetVersion function.\n", ERR_MEMORY);
 
   VersionStruct->Memory = malloc(1);
@@ -81,7 +84,7 @@ int8_t GetCDNVersion(MemoryStruct *VersionStruct) {
 
   Response = CurlGet(VersionStruct, FullURL);
 
-  if (Response != CURLE_OK) { 
+  if (unlikely(Response != CURLE_OK)) { 
     Error("[ERROR]: GetCDNVersion failed with curl error.\n", ERR_STANDARD | ERR_NOEXIT);
     Error((char *)curl_easy_strerror(Response), ERR_STANDARD | ERR_NOEXIT);
     free(FullURL);
