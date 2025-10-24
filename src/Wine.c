61e480faed443881c62adebf5dee66daf2ffa5db
SuperPuiu <SuperPuiu@proton.me>
2025-10-24 19:00:28 +0300

    wine: remove useless whitespaces and make use of ExecProgram

--- src/Wine.c
+++ src/Wine.c
@@ -31,7 +31,7 @@ static char *GetPrefixPath(uint32_t ExtraBytes) {
   uint32_t PrefixLength = strlen(PREFIX);
 
   char *Location = malloc((HomeLength + InstallDirLength + PrefixLength + ExtraBytes + 4) * sizeof(char));
-  
+
   if (unlikely(!Location))
     Error("[FATAL]: Unable to allocate Location buffer during GetPrefixPath call.", ERR_MEMORY);
 
@@ -47,27 +47,27 @@ static char *GetPrefixPath(uint32_t ExtraBytes) {
 /* AddNewUser() adds a new account to the registry, allowing the user to log into it.
  * The registry is HKEY_CURRENT_USER/Software/Roblox/RobloxStudio/LoggedInUserStore/https:/www.roblox.com/users.
  * Format: {"userId1":{"username1":"name","profilePicUrl":"url"}}; {"userId2":{"username":"name","profilePicUrl":"url"}}; ...
- * The format userId should be the account's userid, the name can be anything and the url must be **ANY** valid url to an image. 
+ * The format userId should be the account's userid, the name can be anything and the url must be **ANY** valid url to an image.
  *
  * When first adding the account to the registry key and the user switches to said account, they'll be prompted to use browser login.
  * After that in theory it **should** work. User should close both studio windows after logging into the new account.
  *
  * @return 0 on success and -1 on failure.*/
-int8_t AddNewUser(char *UserId, char *Name, char *URL) {
+int8_t AddNewUser(char *restrict UserId, char *restrict Name, char *restrict URL) {
   const char *NEW_USER = "{\\\"%s\\\":{\\\"username\\\":\\\"%s\\\",\\\"profilePicUrl\\\":\\\"%s\\\"}};";
 
   char *StrtolEndChar = NULL;
   strtol(UserId, &StrtolEndChar, 10);
-  
+
   if (errno != 0 || UserId == NULL || *StrtolEndChar) {
     Error("[ERROR]: UserId is not a valid number.", ERR_STANDARD | ERR_NOEXIT);
     return -1;
   }
-  
+
   URL = URL ? URL : "";
-  
+
   FILE *UserRegistry;
-  
+
   const char *RegistryFileName = "user.reg";
   const char *KeyLocation = "[Software\\\\Roblox\\\\RobloxStudio\\\\LoggedInUsersStore\\\\https:\\\\www.roblox.com]";
 
@@ -79,12 +79,12 @@ int8_t AddNewUser(char *UserId, char *Name, char *URL) {
   char *Prefix = GetPrefixPath(RegFileLen + 1);
   char *Buffer = malloc(NewBufferLen);
   char *KeyContent = NULL, *RegFileContent = NULL, *KeyStart = NULL;
-  
+
   if (unlikely(!Buffer))
     Error("[ERROR]: Unable to allocate Buffer during AddNewUser call.", ERR_MEMORY);
-  
+
   sprintf(Buffer, NEW_USER, UserId, Name, URL);
-  
+
   memcpy(Prefix + strlen(Prefix), RegistryFileName, RegFileLen + 1);
   UserRegistry = fopen(Prefix, "r+");
 
@@ -96,12 +96,12 @@ int8_t AddNewUser(char *UserId, char *Name, char *URL) {
   fseek(UserRegistry, 0, SEEK_END);
   RegFileContentSize = ftell(UserRegistry);
   fseek(UserRegistry, 0, SEEK_SET);
-  
+
   RegFileContent = malloc((RegFileContentSize + NewBufferLen + 1) * sizeof(char));
-  
+
   if (unlikely(!RegFileContent))
     Error("[ERROR]: Unable to allocate RegFileContent during AddNewUser call.", ERR_MEMORY);
-  
+
   fread(RegFileContent, sizeof(char), RegFileContentSize, UserRegistry);
   RegFileContent[RegFileContentSize] = '\0';
 
@@ -111,7 +111,7 @@ int8_t AddNewUser(char *UserId, char *Name, char *URL) {
     Error("[ERROR]: Unable to find the KeyStart in registry.", ERR_STANDARD | ERR_NOEXIT);
     goto error;
   }
-  
+
   /* Hope we don't hit the wrong key.. */
   KeyStart = strstr(KeyStart, "users");
   if (unlikely(!KeyStart)) {
@@ -120,23 +120,23 @@ int8_t AddNewUser(char *UserId, char *Name, char *URL) {
   }
 
   KeyStart += 7;
-  
+
   do { OldKeyLen++; } while (KeyStart[OldKeyLen] != '\n');
-  
+
   KeyContent = malloc((OldKeyLen + NewBufferLen + 1) * sizeof(char));
-  
+
   if (unlikely(!KeyContent))
     Error("[ERROR]: Unable to allocate KeyContent during AddNewUser call.", ERR_MEMORY);
-  
+
   memcpy(KeyContent, KeyStart, OldKeyLen);
-  
+
   memcpy(KeyContent + OldKeyLen - 1, Buffer, NewBufferLen - 1);
   KeyContent[OldKeyLen + NewBufferLen - 2] = '"';
   KeyContent[OldKeyLen + NewBufferLen - 1] = '\0';
-  
+
   memmove(KeyStart + OldKeyLen + NewBufferLen - 1, KeyStart + OldKeyLen + 1, strlen(KeyStart) - OldKeyLen);
   memcpy(KeyStart, KeyContent, OldKeyLen + NewBufferLen - 1);
-  
+
   fseek(UserRegistry, 0, SEEK_SET);
   fwrite(RegFileContent, sizeof(char), RegFileContentSize + (NewBufferLen - OldKeyLen), UserRegistry);
 
@@ -152,10 +152,10 @@ error:
 
   free(Prefix);
   free(Buffer);
-  
+
   if (RegFileContent)
     free(RegFileContent);
-  
+
   if (KeyContent)
     free(KeyContent);
 
@@ -173,50 +173,50 @@ char *GetStudioSetting(char *Name) {
 
   const char *MainSettingsFile = "/drive_c/users/steamuser/AppData/Local/Roblox/GlobalSettings_13.xml";
   MSFLen = strlen(MainSettingsFile);
-  
+
   char *FileData = NULL;
   char *SettingState = NULL;
   char *FullPath = GetPrefixPath(MSFLen);
-  
+
   FullPathLen = strlen(FullPath);
   memcpy(FullPath + FullPathLen, MainSettingsFile, MSFLen + 1);
-  
+
   Settings = fopen(FullPath, "r");
-  
+
   if (!Settings) {
     Error("[ERROR]: Unable to open %s during GetStudioSetting call.", ERR_STANDARD | ERR_NOEXIT, FullPath);
     goto error;
   }
-  
+
   fseek(Settings, 0, SEEK_END);
   FileDataLen = ftell(Settings);
   fseek(Settings, 0, SEEK_SET);
-  
+
   FileData = malloc((FileDataLen + 1) * sizeof(char));
-  
+
   if (!FileData)
     Error("[ERROR]: Unable to allocate FileData during GetStudioSetting call.", ERR_MEMORY);
-  
+
   fread(FileData, sizeof(char), FileDataLen, Settings);
   FileData[FileDataLen] = '\0';
-  
+
   char *KeyStart = strstr(FileData, Name);
-  
+
   if (!KeyStart) {
     Error("[ERROR]: Unable to find %s.", ERR_STANDARD | ERR_NOEXIT, Name);
     goto error;
   }
 
   KeyStart += NameLen;
-  
+
   if (*KeyStart != '"') {
     Error("[ERROR]: KeyStart appears to not point to the end of a string.", ERR_STANDARD | ERR_NOEXIT);
     goto error;
   }
-  
+
   /* Skip the end quote and arrow. */
   KeyStart += 2;
-  
+
   /* I've chosen 6 to be the default size as that's the required size to store "false\0" */
   SettingState = malloc(6 * sizeof(char));
 
@@ -225,10 +225,10 @@ char *GetStudioSetting(char *Name) {
       StateSize *= 1.5;
 
       char *NewPtr = realloc(SettingState, StateSize);
-      
+
       if (!NewPtr)
         Error("[ERROR]: Unable to realloc SettingsState.", ERR_MEMORY);
-      
+
       SettingState = NewPtr;
     }
 
@@ -261,9 +261,9 @@ int8_t SetupWine(uint8_t CheckExistence) {
   const char *PROTON_LINK = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton10-17/GE-Proton10-17.tar.gz";
   const char *PROTON_DIR = "proton";
   char *UpdateWine = PwootieReadEntry("update_wine");
-  
+
   printf("[INFO]: Setting up wine.\n");
-  
+
   if (UpdateWine) {
     if (strcmp(UpdateWine, "false") == 0 && CheckExistence == 1) {
       printf("[INFO]: Option update_wine appears to be false. Aborting installation.\n");
@@ -274,12 +274,12 @@ int8_t SetupWine(uint8_t CheckExistence) {
     free(UpdateWine);
   }
 
-  /* Unfortunately for me, the file is a tar.xz, which means that I'd either have to 
+  /* Unfortunately for me, the file is a tar.xz, which means that I'd either have to
    * introduce ANOTHER dependency to Pwootie only for this whole case, or call system().
    * Second choice sounds like the best choice to me. */
   uint32_t HomeLength = strlen(getenv("HOME")), InstallDirLength = strlen(INSTALL_DIR);
   uint32_t ProtonDirLength = strlen(PROTON_DIR), ProtonNameLength = strlen(PROTON_NAME);
-  
+
   CURLcode Response;
 
   /* 3 for two additional slashes and one additional magic byte.
@@ -306,7 +306,7 @@ int8_t SetupWine(uint8_t CheckExistence) {
     free(Command);
     return 0;
   }
-  
+
   memcpy(PathCopy, Path, Total);
 
   memcpy(Path + HomeLength + InstallDirLength + ProtonDirLength + 3, PROTON_NAME, ProtonNameLength);
@@ -314,12 +314,12 @@ int8_t SetupWine(uint8_t CheckExistence) {
 
   /* Open file and download it. */
   TarFile = fopen(Path, "w");
-  
+
   if (unlikely(!TarFile)) {
     Error("[ERROR]: Failed to open TarFile which is required to download proton.", ERR_STANDARD | ERR_NOEXIT);
     goto error;
   }
-  
+
   printf("[INFO]: Downloading %s.\n", (char*)PROTON_LINK);
   Response = CurlDownload(TarFile, (char*)PROTON_LINK);
   // Response = CURLE_OK;
@@ -328,29 +328,29 @@ int8_t SetupWine(uint8_t CheckExistence) {
     Error("[ERROR]: Failed to download the tar file. (cURL error: %s)", ERR_STANDARD | ERR_NOEXIT, curl_easy_strerror(Response));
     goto error;
   }
-  
+
   fclose(TarFile);
 
   /* im not going to memcpy this. */
   printf("[INFO]: Unzipping new version.\n");
   sprintf(Command, "tar -xvf %s -C %s > /dev/null 2>&1", Path, PathCopy);
   system(Command);
-  
+
   /* Remove zip file. */
   remove(Path);
-  
+
   /* Write wine_binary entry. If wine64 binary can't be found, then throw an error. */
   Path[HomeLength + InstallDirLength + ProtonNameLength + ProtonDirLength - 4] = '\0';
   nftw(Path, Search, 10, FTW_DEPTH | FTW_MOUNT | FTW_PHYS);
-  
+
   if (unlikely(BinPath[0] == 0)) {
     Error("[ERROR]: Unable to find wine64 binary. Invalid installation path (%s).", ERR_STANDARD | ERR_NOEXIT, Path);
     goto error;
   }
-  
+
   printf("%s\n", BinPath);
   PwootieWriteEntry("wine_binary", (char*)BinPath);
-  
+
   /* Cleanup. */
   free(Command);
   free(Path);
@@ -372,7 +372,7 @@ error:
  * @return 0 on success, otherwise return -1 on failure. */
 int8_t SetupPrefix() {
   printf("[INFO]: Setting up prefix.\n");
-  
+
   char *WineBinary = PwootieReadEntry("wine_binary");
   uint8_t FreePath = 1;
 
@@ -380,7 +380,7 @@ int8_t SetupPrefix() {
     WineBinary = "";
     FreePath = 0;
   }
-  
+
   setenv("WINE", WineBinary, 1);
 
   if (unlikely(system("winetricks --version > /dev/null 2>&1") != 0)) {
@@ -391,7 +391,7 @@ int8_t SetupPrefix() {
   int32_t Status;
   /* Is all this string building needed? */
   char *Location = GetPrefixPath(0);
-  
+
   if (mkdir(Location, 0755) && errno != EEXIST) {
     Error("[FATAL]: Unable to create folder during SetupPrefix call.", ERR_STANDARD | ERR_NOEXIT);
     Error(Location, ERR_STANDARD | ERR_NOEXIT);
@@ -400,7 +400,7 @@ int8_t SetupPrefix() {
 
   /* https://www.man7.org/linux/man-pages/man3/setenv.3.html */
   setenv(WINEPREFIX, Location, 1);
-  
+
   /* Install required dlls for studio to launch. Check status values. */
   printf("[INFO]: Installing d3dx11_43.\n");
   Status = system("winetricks d3dx11_43 > /dev/null 2>&1");
@@ -408,14 +408,14 @@ int8_t SetupPrefix() {
     Error("[FATAL]: winetricks failed to install d3dx11_43. Please try to manually install the component.", ERR_STANDARD | ERR_NOEXIT);
     goto error;
   }
-  
+
   printf("[INFO]: Installing dxvk.\n");
   Status = system("winetricks dxvk > /dev/null 2>&1");
   if (unlikely(Status != 0)) {
     Error("[FATAL]: winetricks failed to install dxvk. Please try to manually install the component.", ERR_STANDARD | ERR_NOEXIT);
     goto error;
   }
-  
+
   if (FreePath)
     free(WineBinary);
 
@@ -440,7 +440,7 @@ void RunWineCfg() {
   if (!WineExec) {
     PwootieWriteEntry("wine_binary", "wine");
     WineExec = malloc((strlen("wine") + 1) * sizeof(char));
-    
+
     if (!WineExec)
       Error("[FATAL]: Unable to allocate memory for WINE_EXEC.", ERR_MEMORY);
   }
@@ -450,7 +450,7 @@ void RunWineCfg() {
 
   if (!Command)
     Error("[FATAL]: Unable to allocate Command during RunWineCfg call.", ERR_MEMORY);
-  
+
   memcpy(Command, WineExec, WineExecLen);
 
   for (uint32_t SrcIndex = WineExecLen; SrcIndex > 0; SrcIndex--) {
@@ -462,7 +462,8 @@ void RunWineCfg() {
   }
 
   setenv(WINEPREFIX, Prefix, 1);
-  if (unlikely(system(Command) != 0)) {
+
+  if (unlikely(ExecProgram(Command, 0) != 0)) {
     Error("[ERROR]: system(%s) returned non zero value. Running default winecfg.", ERR_STANDARD | ERR_NOEXIT, Command);
     if (unlikely(system("winecfg") != 0)) /* Fallback. */
       Error("[ERROR]: Unable to run fallback winecfg. Aborting.", ERR_STANDARD | ERR_NOEXIT);
@@ -473,17 +474,16 @@ void RunWineCfg() {
   free(WineExec);
 }
 
-void Run(char *Argument, char *Version) {
-  const char *EXECUTABLE ="RobloxStudioBeta.exe";
+void Run(char *restrict Argument, char *restrict Version) {
+  const char *EXECUTABLE = "RobloxStudioBeta.exe";
 
   if (Argument == NULL)
     Argument = "";
-  
+
   /* Yeah well I can't put them next to the other char arrays. */
   char *WINE_EXEC = PwootieReadEntry("wine_binary");
   char *DEBUG_ENTRY = PwootieReadEntry("debug");
-  char *EXTRA_CMD = " > /dev/null 2>&1 & disown";
-  uint8_t FreeFlag = 1;
+  uint8_t FreeFlag = 1, Silence = 1;
 
   /* If the wine_binary entry doesn't exist, just create a dummy one and hope wine exists. */
   if (!WINE_EXEC) {
@@ -491,26 +491,21 @@ void Run(char *Argument, char *Version) {
     FreeFlag = 0;
     WINE_EXEC = "wine";
   }
-  
+
   if (DEBUG_ENTRY) {
     if (strcmp(DEBUG_ENTRY, "true") == 0)
-      EXTRA_CMD = "& disown";
+      Silence = 0;
+
+    free(DEBUG_ENTRY);
   }
 
-  uint32_t WineLen = strlen(WINE_EXEC), ArgLen = strlen(Argument);
   uint32_t VersionLen = strlen(Version), HomeLength = strlen(getenv("HOME"));
   uint32_t InstallLen = strlen(INSTALL_DIR), ExecutableLen = strlen(EXECUTABLE);
-  uint32_t ExtraCmdLen = strlen(EXTRA_CMD);
-  uint32_t ExecPathLen = HomeLength + InstallLen + VersionLen + ExecutableLen + 4;
-  
-  /* Command needs extra space for the quotes required by the arguments. We may also need an additional dot. */
+
   char *Location = GetPrefixPath(0);
-  char *Command = malloc((WineLen + ArgLen + ExecPathLen + ExtraCmdLen + 2 + 3) * sizeof(char));
-  char *Executable = malloc(ExecPathLen * sizeof(char));
-  
-  if (unlikely(!Command))
-    Error("[FATAL]: Unable to allocate Command buffer during Run call.", ERR_MEMORY);
-  else if (unlikely(!Executable))
+  char *Executable = malloc((HomeLength + InstallLen + VersionLen + ExecutableLen + 4) * sizeof(char));
+
+  if (unlikely(!Executable))
     Error("[FATAL]: Unable to allocate Executable buffer during Run call.", ERR_MEMORY);
 
   memcpy(Executable, getenv("HOME"), HomeLength);
@@ -519,17 +514,8 @@ void Run(char *Argument, char *Version) {
   memcpy(Executable + HomeLength + InstallLen + VersionLen + 3, EXECUTABLE, ExecutableLen);
   Executable[HomeLength] = Executable[HomeLength + InstallLen + 1] = Executable[HomeLength + InstallLen + VersionLen + 2] = '/';
 
-  memcpy(Command, WINE_EXEC, WineLen);
-  memcpy(Command + WineLen + 1, Executable, ExecPathLen);
-  memcpy(Command + WineLen + ExecPathLen + 2, Argument, ArgLen);
-  memcpy(Command + WineLen + ArgLen + ExecPathLen + 3, EXTRA_CMD, ExtraCmdLen);
-  Command[WineLen] = Command[WineLen + ExecPathLen] = ' ';
-  Command[WineLen + ExecPathLen + 1] = Command[WineLen + ArgLen + ExecPathLen + 2] = '"';
-  Command[WineLen + ArgLen + ExecPathLen + ExtraCmdLen + 3] = '\0';
-
-  /* Apparently system() can have some security vulnerabilities? eh? */
   setenv(WINEPREFIX, Location, 1);
-  system(Command);
+  ExecProgram(WINE_EXEC, Silence, Executable, Argument, NULL);
 
   free(Executable);
   free(Location);
